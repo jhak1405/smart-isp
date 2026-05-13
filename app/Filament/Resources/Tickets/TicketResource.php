e49bd3c48ac84e9eef2d286cd7c65a5756048e1c
jhak1405 <rivas250n@gmail.com>
2026-05-13 13:51:05 -0500

    animacion de desplazamiento en editar ticket

--- app/Filament/Resources/Tickets/TicketResource.php
+++ app/Filament/Resources/Tickets/TicketResource.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\Tickets;
 
 use App\Filament\Resources\Tickets\Pages\CreateTicket;
-use App\Filament\Resources\Tickets\Pages\EditTicket;
 use App\Filament\Resources\Tickets\Pages\ListTickets;
 use App\Filament\Resources\Tickets\Pages\ViewTicket;
 use App\Filament\Resources\Tickets\Schemas\TicketForm;
@@ -78,7 +77,6 @@ class TicketResource extends Resource
             'index'  => ListTickets::route('/'),
             'create' => CreateTicket::route('/create'),
             'view'   => ViewTicket::route('/{record}'),
-            'edit'   => EditTicket::route('/{record}/edit'),
         ];
     }
 }
